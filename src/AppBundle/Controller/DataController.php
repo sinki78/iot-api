4c3f68288837907d56d316ed81dfa297ed5d45da
Clément Gaudron <clement.gaudron@hotmail.com>
2017-04-06 16:12:57 +0200

    add post suite

--- src/AppBundle/Controller/DataController.php
+++ src/AppBundle/Controller/DataController.php
@@ -8,6 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 
 /**
@@ -88,42 +89,45 @@ class DataController extends Controller
         if (!empty($content))
         {
             $params = json_decode($content, true); // 2nd param to get as array
-        }
 
-        foreach ($params as $line){
-            foreach ($line as $key=>$value){
+            foreach ($params as $line){
+                foreach ($line as $key=>$value){
 
-                $CO2 = new Data();
-                $PF = new Data();
-                switch ($key){
-                    case 'date':
-                        $CO2->setDate($value);
-                        $PF->setDate($value);
-                        break;
-                    case 'lon':
-                        $CO2->setLongitude($value);
-                        $PF->setLongitude($value);
-                        break;
-                    case 'lat':
-                        $CO2->setLatitude($value);
-                        $PF->setLatitude($value);
-                        break;
-                    case 'co2':
-                        $CO2->setType($key);
-                        $CO2->setValeur($value);
-                        break;
-                    case 'pf':
-                        $PF->setType($key);
-                        $PF->setValeur($value);
-                        break;
+                    $CO2 = new Data();
+                    $PF = new Data();
+                    switch ($key){
+                        case 'date':
+                            $transitionToDst=$value;
+                            $date = new DateTime($transitionToDst, new \DateTimeZone('Europe/Paris'));
+                            $CO2->setDate($date);
+                            $PF->setDate($date);
+                            break;
+                        case 'lon':
+                            $CO2->setLongitude($value);
+                            $PF->setLongitude($value);
+                            break;
+                        case 'lat':
+                            $CO2->setLatitude($value);
+                            $PF->setLatitude($value);
+                            break;
+                        case 'co2':
+                            $CO2->setType($key);
+                            $CO2->setValeur($value);
+                            break;
+                        case 'pf':
+                            $PF->setType($key);
+                            $PF->setValeur($value);
+                            break;
 
+                    }
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($CO2);
+                    $em->persist($PF);
+                    $em->flush();
                 }
-                $em = $this->getDoctrine()->getManager();
-                $em->persist($CO2);
-                $em->persist($PF);
-                $em->flush();
             }
         }
+
         return new JsonResponse('success');
     }
 
